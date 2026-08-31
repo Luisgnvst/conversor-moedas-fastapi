52e6b17eb716d96a0d34f6510011c19c8c568765
Luis Gabriel Neves Tristao <luisgabrielnevest@gmail.com>
2026-08-31 15:48:21 -0300

    Implementa cálculo de conversão com tabela de taxas fixa

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"mensagem": "Minha primeira API"}
+taxas = {
+    "USD-BRL": 5.40,
+    "BRL-USD": 0.185,
+    "EUR-BRL": 5.85
+}
+
+@app.get("/converter")
+def converter(valor: float, de: str, para: str):
+    chave = f"{de}-{para}"
+    resultado = taxas[chave] * valor
+    return {"resultado": resultado}
+
